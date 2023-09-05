a260c7d5dcd00b78a6b69e5b485e8e091f5d0196
misantamaria <mi.santamaria@upm.es>
2023-09-05 09:28:38 +0000

    mejorado get_jupyter_token

--- docker/get_jupyter_token.sh
+++ docker/get_jupyter_token.sh
@@ -3,4 +3,4 @@ ip="127.0.0.1"
 nombre="g3.etsisi.upm.es"
 puerto_original="8888"
 puerto_final="8866"
-docker compose logs jupyter | grep "token=" | tail -n 1 | sed -e "s/$ip/$nombre/" -e "s/$puerto_original/$puerto_final/" | sed "s/.*\(http\)/\1/"
+docker compose logs jupyter | grep "token=" | tail -n 1 | sed -e "s/$ip/$nombre/" -e "s/$puerto_original/$puerto_final/" | sed "s/.*\(http\)/\1/" | sed -e 's/.*=//'

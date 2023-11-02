2bf009e92e227fe2ab3d352bf519e47210dca5dc
misantamaria@upm.es <mi.santamaria@upm.es>
2023-11-02 10:54:43 +0000

    version para cuda 12_2_0 con pytorch_1_13 cudf 23.02 cuml 23.02

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash --login
 echo "About to exec deepvats\docker\entrypoint.sh"
+
 set -e
+#conda list --prefix $ENV_PREFIX
 conda activate $ENV_PREFIX
+
+#echo $ENV_PREFIX
+#echo $PATH
+#conda list 
 pip install -e /home/$USER/work
 exec "$@"

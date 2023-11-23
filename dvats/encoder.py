773ec71dc1f73381ccb4ae936e794a01c9e979fa
misantamaria@upm.es <mi.santamaria@upm.es>
2023-11-23 15:06:00 +0000

    fixed get_enc_embedings in dvats when embs dont fit in GPU. Fixed 03b embeddings sliding window

--- dvats/encoder.py
+++ dvats/encoder.py
@@ -9,6 +9,10 @@ Original file is located at:
 
 #default_exp encoder
 
+#hide
+#%load_ext autoreload --> Not working TODO:REVISAR
+# %autoreload 2 
+
 #export
 import pandas as pd
 import numpy as np
@@ -64,11 +68,6 @@ class DCAE_torch(Module):
         x = self.upsample(x)
         return x
 
-#hide
-foo = torch.rand(3, 1, 48)
-m = DCAE_torch(c_in=foo.shape[1], seq_len=foo.shape[2], delta=12)
-m(foo).shape
-
 #export
 ENCODER_EMBS_MODULE_NAME = {
     InceptionTimePlus: 'backbone', # for mvp based models
@@ -86,64 +85,51 @@ def get_enc_embs(X, enc_learn, module=None, cpu=False, average_seq_dim=True, to_
         - `average_seq_dim`: Whether to aggregate the embeddings in the sequence dimensions
         - `to_numpy`: Whether to return the result as a numpy array (if false returns a tensor)
     """
+    # Ensure empty cache
+    torch.cuda.empty_cache()
+    ######################
     if cpu:
         print("--> Get enc embs CPU")
         enc_learn.dls.cpu()
         enc_learn.cpu()
     else:
-        print("--> Use CUDA |Get enc embs GPU")
+        print("--> Use CUDA |Get enc embs GPU ")
         enc_learn.dls.cuda()
         enc_learn.cuda()
-        print("device: ", enc_learn.model.parameters())
-        print("Use CUDA -->")
+        if torch.cuda.is_available():
+            print("CUDA está disponible")
+            print("Dispositivo CUDA actual: ", torch.cuda.current_device())
+            print("Nombre del dispositivo CUDA actual: ", torch.cuda.get_device_name(torch.cuda.current_device()))
+
+        else:
+            print("CUDA no está disponible ")
+            print("Use CUDA -->")
     if enc_learn.dls.bs == 0: enc_learn.dls.bs = 64
-    print("--> Get enc embs bs: ", enc_learn.dls.bs)
+
+    print("--> Set dataset from X (enc_learn does not contain dls)")
     aux_dl = enc_learn.dls.valid.new_dl(X=X)
     aux_dl.bs = enc_learn.dls.bs if enc_learn.dls.bs>0 else 64
+    print("--> Get module")
     module = nested_attr(enc_learn.model,
                          ENCODER_EMBS_MODULE_NAME[type(enc_learn.model)]) \
                 if module is None else module
+    #Get embeddings
+    print("--> Get enc embs bs: ", aux_dl.bs)
     embs = [get_acts_and_grads(model=enc_learn.model,
                                modules=module,
                                x=xb[0], cpu=cpu)[0] for xb in aux_dl]
+    print("--> Concat")
+    if not cpu:
+        total_emb_size = sum([emb.element_size() * emb.nelement() for emb in embs])
+        free_memory = torch.cuda.get_device_properties(0).total_memory - torch.cuda.memory_allocated()
+        if (total_emb_size < free_memory):
+            print("Fit in GPU")
+            embs=[emb.cuda() for emb in embs]
+        else:
+            embs=[emb.cpu() for emb in embs]
     embs = to_concat(embs)
+    print("--> reduce")
     if embs.ndim == 3 and average_seq_dim: embs = embs.mean(axis=2)
+    print("--> 2 numpy")
     if to_numpy: embs = embs.numpy() if cpu else embs.cpu().numpy()
     return embs
-
-#hide
-import wandb
-from dvats.utils import *
-wandb_api = wandb.Api()
-enc_artifact = wandb_api.artifact('deepvats/mvp:latest')
-enc_learner = enc_artifact.to_obj()
-X = torch.rand(9, 1, 48)
-
-#hide
-#slow
-#%%time
-embs = get_enc_embs(X, enc_learner, cpu=True)
-test_eq(embs.shape[0], X.shape[0])
-embs.shape, embs.__class__
-
-#hide
-%%time
-embs = get_enc_embs(X, enc_learner, cpu=False, to_numpy=False)
-test_eq(embs.shape[0], X.shape[0])
-embs.shape, embs.__class__, embs.device
-
-#hide 
-%%time
-embs = get_enc_embs(X, enc_learner, cpu=False, to_numpy=True)
-test_eq(embs.shape[0], X.shape[0])
-embs.shape, embs.__class__
-
-#hide
-
-#from nbdev.export import notebook2script
-
-#notebook2script()
-
-#from tsai import nb2py
-#nb2py
-#beep(1)

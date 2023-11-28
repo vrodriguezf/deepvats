95bfa5e445e3b2ded18e791bb061d8c36cc20160
misantamaria@upm.es <mi.santamaria@upm.es>
2023-11-28 16:01:11 +0000

    nbdev_migrate applied

--- r_shiny_app/server.R
+++ r_shiny_app/server.R
@@ -773,7 +773,7 @@ shinyServer(function(input, output, session) {
         plt <- ggplot(data = prjs_) + 
             aes(x = xcoord, y = ycoord, fill = highlight, color = as.factor(cluster)) + 
             scale_colour_manual(name = "clusters", values = req(update_palette())) +
-            geom_point(shape = 21,alpha = config_style$point_alpha, size = config_style$point_size) + 
+            geom_point(shape = 21,alpha = config_style$point_alpha, linewidth = config_style$point_size) + 
             scale_shape(solid = FALSE) +
             geom_path(size=config_style$path_line_size, colour = "#2F3B65",alpha = config_style$path_alpha) + 
             guides() + 

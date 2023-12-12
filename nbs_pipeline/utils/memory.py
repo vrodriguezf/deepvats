8208fc062b38c0d6a3f6d73a67e7153ea7b7041b
misantamaria@upm.es <mi.santamaria@upm.es>
2023-12-12 16:19:01 +0000

    añadidas funciones de lectura de memoria en cpu en nbs_pipeline/utils para analizar por qué se bloquea la aplicación

--- nbs_pipeline/utils/memory.py
+++ nbs_pipeline/utils/memory.py
@@ -1,4 +1,14 @@
+# -*- coding: utf-8 -*-
+"""memory.ipynb
+
+Automatically generated.
+
+Original file is located at:
+    /home/macu/work/nbs_pipeline/utils_nbs/memory.ipynb
+"""
+
 import subprocess
+import psutil
 
 def get_gpu_memory(device=0):
     total_memory = subprocess.check_output(["nvidia-smi", "--query-gpu=memory.total", "--format=csv,noheader,nounits", "--id=" + str(device)])
@@ -8,7 +18,6 @@ def get_gpu_memory(device=0):
 
     percentage = round((used_memory / total_memory) * 100)
     return used_memory, total_memory, percentage
-
 
 def color_for_percentage(percentage):
     if percentage < 20:
@@ -21,7 +30,7 @@ def color_for_percentage(percentage):
         return "\033[93m"  # Orange
     else:
         return "\033[91m"  # Red
-        
+
 def create_bar(percentage, color_code, length=20):
     filled_length = int(length * percentage // 100)
     bar = "█" * filled_length + "-" * (length - filled_length)
@@ -34,3 +43,22 @@ def gpu_memory_status(device=0):
     print(f"Used mem: {used}")
     print(f"Used mem: {total}")
     print(f"Memory Usage: [{bar}] {color_code}{percentage}%\033[0m")
+
+gpu_memory_status()
+
+def get_cpu_memory():
+    mem = psutil.virtual_memory()
+    total_memory = mem.total // (1024**2)  # Convertir a MB
+    used_memory = (mem.total - mem.available) // (1024**2)  # Convertir a MB
+    percentage = mem.percent
+    return used_memory, total_memory, percentage
+
+def cpu_memory_status():
+    used, total, percentage = get_cpu_memory()
+    color_code = color_for_percentage(percentage)
+    bar = create_bar(percentage, color_code)
+    print(f"Used mem: {used}")
+    print(f"Used mem: {total}")
+    print(f"Memory Usage: [{bar}] {color_code}{percentage}%\033[0m")
+
+cpu_memory_status()

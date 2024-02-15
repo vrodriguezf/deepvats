78266e8b86654390a2413ea1c89aaea687a93f9f
mi.santamaria <mi.santamaria@upm.es>
2024-02-15 13:25:57 +0000

    setup.py header adapted to importlib.metadata

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from pkg_resources import parse_version
+from importlib.metadata import version
 from configparser import ConfigParser
+
 import setuptools
-assert parse_version(setuptools.__version__)>=parse_version('36.2')
+
+setuptools_version = version('setuptools')
+assert setuptools_version >= '36.2'
 
 # note: all settings are in settings.ini; edit there, not here
 config = ConfigParser(delimiters=['='])

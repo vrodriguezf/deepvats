4e0f66667867d163db8e719c2b0d858963d47db3
misantamaria@upm.es <mi.santamaria@upm.es>
2024-02-15 13:13:29 +0000

    trying to build up the docker from the other user

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from pkg_resources import parse_version
+from importlib.metadata import version
 from configparser import ConfigParser
-import setuptools
-assert parse_version(setuptools.__version__)>=parse_version('36.2')
 
+import setuptools
+
+setuptools_version = version('setuptools')
+assert setuptools_version >= '36.2'
 # note: all settings are in settings.ini; edit there, not here
 config = ConfigParser(delimiters=['='])
 config.read('settings.ini')

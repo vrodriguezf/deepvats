d5107e0fd285e3e80dc0c9b5f96cb46d301e9729
misantamaria@upm.es <mi.santamaria@upm.es>
2024-02-16 14:22:50 +0000

    removed extra files

--- docker/change_docker.sh
+++ docker/change_docker.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
 if [ -f "docker-compose.yml" ]; then
-    mv "docker-compose.yml" "docker_compose.yml__"
+    mv "docker-compose.yml" "docker-compose.yml__"
 fi
 
-if [ -f "docker_compose.yml_" ]; then
-    mv "docker_compose.yml_" "docker_compose.yml"
+if [ -f "docker-compose.yml_" ]; then
+    mv "docker-compose.yml_" "docker-compose.yml"
 fi
 
-if [ -f "docker_compose.yml__" ]; then
-    mv "docker_compose.yml__" "docker_compose.yml_"
+if [ -f "docker-compose.yml__" ]; then
+    mv "docker-compose.yml_" "docker-compose.yml_"
 fi
 
 if [ -f "Dockerfile.jupyter" ]; then
-    mv "Dockerfile.jupyter" "Dockerfile_jupyter__"
+    mv "Dockerfile.jupyter" "Dockerfile.jupyter__"
 fi
 
-if [ -f "Dockerfile_jupyter_" ]; then
-    mv "Dockerfile_jupyter_" "Dockerfile.jupyter"
+if [ -f "Dockerfile.jupyter_" ]; then
+    mv "Dockerfile-jupyter_" "Dockerfile.jupyter"
 fi
 
-if [ -f "Dockerfile_jupyter__" ]; then
-    mv "Dockerfile_jupyter__" "Dockerfile_jupyter_"
+if [ -f "Dockerfile.jupyter__" ]; then
+    mv "Dockerfile.jupyter__" "Dockerfile.jupyter_"
 fi
 
 if [ -f "Dockerfile.rstudio" ]; then
-    mv "Dockerfile.rstudio" "Dockerfile_rstudio__"
+    mv "Dockerfile.rstudio" "Dockerfile.rstudio__"
 fi
 
-if [ -f "Dockerfile_rstudio_" ]; then
-    mv "Dockerfile_rstudio_" "Dockerfile.rstudio"
+if [ -f "Dockerfile.rstudio_" ]; then
+    mv "Dockerfile.rstudio_" "Dockerfile.rstudio"
 fi
 
-if [ -f "Dockerfile_rstudio__" ]; then
-    mv "Dockerfile_rstudio__" "Dockerfile_rstudio_"
+if [ -f "Dockerfile.rstudio__" ]; then
+    mv "Dockerfile.rstudio__" "Dockerfile.rstudio_"
 fi
 

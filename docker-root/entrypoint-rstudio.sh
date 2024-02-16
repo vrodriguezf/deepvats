f19866f49bbca80161e44407bc8c9c22a74aa74a
misantamaria@upm.es <mi.santamaria@upm.es>
2024-02-16 10:32:07 +0000

    jupyter root docker version

--- docker-root/entrypoint-rstudio.sh
+++ docker-root/entrypoint-rstudio.sh
@@ -1,12 +1,16 @@
 #!/bin/bash --login
 set -e
 
+echo "--> Bashrc"
 echo ". ${HOME}/miniconda3/etc/profile.d/conda.sh" >> ${HOME}/.bashrc
 # Make bash automatically activate the conda environment
+
+echo "--> Conda activate"
 echo "conda activate ${ENV_PREFIX}" >> ~/.bashrc
 #echo "export WANDB_ENTITY=${WANDB_ENTITY:-default}" >> ${HOME}/.bashrc
 # echo "WANDB_ENTITY=${WANDB_ENTITY:-default}" >> ${HOME}/.Renviron
 
+echo "--> Variables"
 # Define an array of environment variable names from the ENV_VARS Compose variable
 IFS=',' read -ra ENV_VAR_NAMES <<< "$ENV_VARS"
 
@@ -18,9 +22,10 @@ for ENV_VAR_NAME in "${ENV_VAR_NAMES[@]}"; do
   echo "$ENV_VAR_NAME=$ENV_VAR_VALUE" >> ${HOME}/.Renviron
 done
 
-
+echo "--> Ulimit"
 ulimit -s 16384
 
+echo "--> Go!"
 # start rstudio server
 /init
 exec "$@"

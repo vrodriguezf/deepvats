787ec456f42d930216caba766563c034c501d17c
misantamaria@upm.es <mi.santamaria@upm.es>
2024-02-18 07:50:11 +0000

    build_image-py.sh read environment variables fixed

--- docker/build_image-py.sh
+++ docker/build_image-py.sh
@@ -2,11 +2,20 @@
 args=()
 
 # Lee el archivo .env línea por línea
-while IFS='=' read -r key value; do
+IFS='='
+while IFS='=' read -r key value _; do
+    value="${value%$'\r'}"
+    echo "key: $key"
+    echo "value: $value"
     if [[ $key != \#* && $key != '' ]]; then  # Excluye comentarios y líneas vacías
         args+=(--build-arg "$key=$value")  # Agrega --build-arg y la variable como un elemento
+    else
+        echo "Skipping $key = $value"
     fi
 done < .env
+
+echo "Args:" "${args[@]}"
+#read -p "Press enter to continue"
 
 # Ejecuta docker build con los argumentos
 PROJECT_NAME='dvats'

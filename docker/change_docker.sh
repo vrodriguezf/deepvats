e555d7404450485fd21c64a642a294904319ede7
misantamaria@upm.es <mi.santamaria@upm.es>
2024-02-19 14:35:34 +0000

    change_docker.sh in english

--- docker/change_docker.sh
+++ docker/change_docker.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-echo "Renombrando docker-compose.yml a docker-compose.yml__"
+echo "Move docker-compose.yml --> docker-compose.yml__"
 mv "docker-compose.yml" "docker-compose.yml__"
 
-echo "Renombrando docker-compose.yml_ a docker-compose.yml"
+echo "Move docker-compose.yml_ --> docker-compose.yml"
 mv "docker-compose.yml_" "docker-compose.yml"
 
-echo "Renombrando docker-compose.yml__ a docker-compose.yml_"
+echo "Move docker-compose.yml__ --> docker-compose.yml_"
 mv "docker-compose.yml__" "docker-compose.yml_"
 
-echo "Renombrando Dockerfile.jupyter a Dockerfile.jupyter__"
+echo "Move Dockerfile.jupyter --> Dockerfile.jupyter__"
 mv "Dockerfile.jupyter" "Dockerfile.jupyter__"
 
-echo "Renombrando Dockerfile.jupyter_ a Dockerfile.jupyter"
+echo "Move Dockerfile.jupyter_ --> Dockerfile.jupyter"
 mv "Dockerfile.jupyter_" "Dockerfile.jupyter"
 
-echo "Renombrando Dockerfile.jupyter__ a Dockerfile.jupyter_"
+echo "Move Dockerfile.jupyter__ --> Dockerfile.jupyter_"
 mv "Dockerfile.jupyter__" "Dockerfile.jupyter_"
 
-echo "Renombrando Dockerfile.rstudio a Dockerfile.rstudio__"
+echo "Move Dockerfile.rstudio --> Dockerfile.rstudio__"
 mv "Dockerfile.rstudio" "Dockerfile.rstudio__"
 
-echo "Renombrando Dockerfile.rstudio_ a Dockerfile.rstudio"
+echo "Move Dockerfile.rstudio_ --> Dockerfile.rstudio"
 mv "Dockerfile.rstudio_" "Dockerfile.rstudio"
 
-echo "Renombrando Dockerfile.rstudio__ a Dockerfile.rstudio_"
+echo "Move Dockerfile.rstudio__ --> Dockerfile.rstudio_"
 mv "Dockerfile.rstudio__" "Dockerfile.rstudio_"

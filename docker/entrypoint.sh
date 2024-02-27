678e8526468c9fd09db17226203ce3eff7ead390
misantamaria@upm.es <mi.santamaria@upm.es>
2024-02-27 11:44:22 +0000

    Chore: pre-commit, git.sh, clean notebooks sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash --login
 set -e
-#echo $ENV_PREFIX
-#conda list 
+
+source /usr/local/share/miniconda3/etc/profile.d/conda.sh
+conda activate /usr/local/share/miniconda3/envs/env
+
+conda list 
 ls -la /home/$USER/work
+
+conda list pre-commit
+
 pip install -e /home/$USER/work
-
-
 
 exec "$@"

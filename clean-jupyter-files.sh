9ffe71cfd870d8461f82fb4bf9558f4255d91c28
misantamaria@upm.es <mi.santamaria@upm.es>
2024-02-28 10:18:43 +0000

    Corregido clear_all en clean-jupyter-files.sh

--- clean-jupyter-files.sh
+++ clean-jupyter-files.sh
@@ -7,7 +7,7 @@ for notebook in "$@"; do
     #/usr/local/share/miniconda3/envs/env/bin/nbdev_clean --clear_all --fname "$notebook"
     #--- Do not clean outputs
     echo "--> Clean $notebook "
-    /usr/local/share/miniconda3/envs/env/bin/nbdev_clean --clear_all --fname "$notebook"
+    /usr/local/share/miniconda3/envs/env/bin/nbdev_clean --fname "$notebook"
     if [ $? -ne 0 ]; then
         echo "Error cleaning $notebook"
     fi

9a016b9ddaf0d02e2003412f53e67587e2987e4b
misantamaria@upm.es <mi.santamaria@upm.es>
2024-03-01 14:34:53 +0000

    asegurado que no peta cuando haya un usuario previamente metido. Intentar que no haya nada cuando se haga un comit....

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,7 +14,7 @@ echo $WANDB_ENTITY $USER $WANDB_PROJECT
 
 
 ########## Fix base.yaml for ensuring correct user & project directly ########## [TODO: Remove this section for directly using .env option]
-line_found=$(grep "^[[:space:]]*user: &wdb_user$" /home/$USER/work/nbs_pipeline/config/base.yaml)
+line_found=$(grep "^[[:space:]]*user: &wdb_user" /home/$USER/work/nbs_pipeline/config/base.yaml)
 
 if [ -z "$line_found" ]; then
     echo "User line not found."
@@ -23,14 +23,15 @@ else
     echo "User line found: '$line_found'"
 
     modified_line="    user: &wdb_user $WANDB_ENTITY"
-    echo "--> Modificando a: '    $modified_line'"
+    echo "--> Modified to: '    $modified_line'"
     
     sed -i "s|^\([[:space:]]*user: &wdb_user\).*|\1 $WANDB_ENTITY|" /home/$USER/work/nbs_pipeline/config/base.yaml
+    
     
     grep "user: &wdb_user" /home/$USER/work/nbs_pipeline/config/base.yaml
 fi
 
-line_found=$(grep "^[[:space:]]*project_name: &wdb_project$" /home/$USER/work/nbs_pipeline/config/base.yaml)
+line_found=$(grep "^[[:space:]]*project_name: &wdb_project" /home/$USER/work/nbs_pipeline/config/base.yaml)
 
 if [ -z "$line_found" ]; then
     echo "Project line not found."
@@ -39,7 +40,7 @@ else
     echo "Project line found: '$line_found'"
 
     modified_line="    project_name: &wdb_project $WANDB_PROJECT"
-    echo "--> Modify toa: '    $modified_line'"
+    echo "--> Modify to: '    $modified_line'"
     
     sed -i "s|^\([[:space:]]*project_name: &wdb_project\).*|\1 $WANDB_PROJECT|" /home/$USER/work/nbs_pipeline/config/base.yaml
     

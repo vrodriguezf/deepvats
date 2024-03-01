5d61cf17e03404c4f432631f9707a722cb62b9c4
misantamaria@upm.es <mi.santamaria@upm.es>
2024-03-01 12:40:05 +0000

    conseguido añadir mi_santamaria

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,24 @@ set -e
 #conda list 
 ls -la /home/$USER/work
 pip install -e /home/$USER/work
+
+echo $WANDB_ENTITY $USER $WANDB_PROJECT
+
+#!/bin/bash
+
+line_found=$(grep "^[[:space:]]*user: &wdb_user$" /home/$USER/work/nbs_pipeline/config/base.yaml)
+
+if [ -z "$line_found" ]; then
+    echo "Línea no encontrada."
+else
+    echo "Línea encontrada: '$line_found'"
+
+    modified_line="    user: &wdb_user $WANDB_ENTITY"
+    echo "--> Modificando a: '    $modified_line'"
+    
+    sed -i "s|^\([[:space:]]*user: &wdb_user\).*|\1 $WANDB_ENTITY|" /home/$USER/work/nbs_pipeline/config/base.yaml
+    
+    grep "user: &wdb_user" /home/$USER/work/nbs_pipeline/config/base.yaml
+fi
+
 exec "$@"

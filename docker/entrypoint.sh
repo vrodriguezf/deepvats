0afc5744dc586f5de9e1244ce8ff0626257bed70
misantamaria@upm.es <mi.santamaria@upm.es>
2024-03-01 12:44:40 +0000

    base yml modification added to script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,12 +9,18 @@ echo $WANDB_ENTITY $USER $WANDB_PROJECT
 
 #!/bin/bash
 
+
+
+
+
+########## Fix base.yaml for ensuring correct user & project directly ########## [TODO: Remove this section for directly using .env option]
 line_found=$(grep "^[[:space:]]*user: &wdb_user$" /home/$USER/work/nbs_pipeline/config/base.yaml)
 
 if [ -z "$line_found" ]; then
-    echo "Línea no encontrada."
+    echo "User line not found."
+    exit 1
 else
-    echo "Línea encontrada: '$line_found'"
+    echo "User line found: '$line_found'"
 
     modified_line="    user: &wdb_user $WANDB_ENTITY"
     echo "--> Modificando a: '    $modified_line'"
@@ -22,6 +28,22 @@ else
     sed -i "s|^\([[:space:]]*user: &wdb_user\).*|\1 $WANDB_ENTITY|" /home/$USER/work/nbs_pipeline/config/base.yaml
     
     grep "user: &wdb_user" /home/$USER/work/nbs_pipeline/config/base.yaml
+fi
+
+line_found=$(grep "^[[:space:]]*project_name: &wdb_project$" /home/$USER/work/nbs_pipeline/config/base.yaml)
+
+if [ -z "$line_found" ]; then
+    echo "Project line not found."
+    exit 1
+else
+    echo "Project line found: '$line_found'"
+
+    modified_line="    project_name: &wdb_project $WANDB_PROJECT"
+    echo "--> Modify toa: '    $modified_line'"
+    
+    sed -i "s|^\([[:space:]]*project_name: &wdb_project\).*|\1 $WANDB_PROJECT|" /home/$USER/work/nbs_pipeline/config/base.yaml
+    
+    grep "project_name: &wdb_project" /home/$USER/work/nbs_pipeline/config/base.yaml
 fi
 
 exec "$@"

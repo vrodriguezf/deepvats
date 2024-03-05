d5957d55d6bbd84d821eb996b43c6e4e955f3832
misantamaria@upm.es <mi.santamaria@upm.es>
2024-03-05 09:56:14 +0000

    execution pipeline checked

---
+++ clean-jupyter-files.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Files received:"
+printf '%s\n' "$@"
+
+for notebook in "$@"; do
+    #--- Clean outputs
+    #/usr/local/share/miniconda3/envs/env/bin/nbdev_clean --clear_all --fname "$notebook"
+    #--- Do not clean outputs
+    echo "--> Clean $notebook "
+    /usr/local/share/miniconda3/envs/env/bin/nbdev_clean --fname "$notebook"
+    if [ $? -ne 0 ]; then
+        echo "Error cleaning $notebook"
+    fi
+    echo "Clean $notebook -->"
+done
+
+exit 0
--- dvats/all.py
+++ dvats/all.py
@@ -2,6 +2,8 @@ import dvats
 from .imports import *
 from .load import *
 from .utils import *
+from .config import *
+from .memory import *
 from .dr import *
 from .encoder import *
 from .visualization import *

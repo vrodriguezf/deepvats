f4a7b1e8840b5d11e310a9a5a673f6bd4db867aa
misantamaria@upm.es <mi.santamaria@upm.es>
2024-03-07 16:42:37 +0000

    Comprobado que funciona

--- docker/check_yml_changes.sh
+++ docker/check_yml_changes.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# Obtain a list of modified .yml files
-MODIFIED_YML_FILES=$(git diff --cached --name-only | grep '\.yml\?$')
+# Obtain a list of modified .yml/.yaml files
+MODIFIED_YML_FILES=$(git diff --cached --name-only | grep -E '\.(yml|yaml)$')
 
-echo "Modified .yml files:"
+echo "Modified files: "$(git diff --cached --name-only)
+echo "Modified yml files: "$MODIFIED_YML_FILES
 # Check if the list is empty
 if [ -z "$MODIFIED_YML_FILES" ]; then
-    read -p "No yaml files modified, proceed with the commit"
+    echo "No yaml files modified, proceed with the commit"
     exit 0
 else
     # .yml files have been modified, ask the user
-    read rp "The following .yml files have been modified:"
+    echo "The following .yml files have been modified:"
     echo "$MODIFIED_YML_FILES"
     read -p "Do you wish to retain the changes in these .yml files? [y/n]: " response
 
     if [[ "$response" =~ ^([nN][oO]|[nN])$ ]]; then
         # The user does not wish to retain the changes, remove the .yml files from the commit
-        read -p "Removing the .yml files from the staging area..."
+        echo "Removing the .yml files from the staging area..."
         for file in $MODIFIED_YML_FILES; do
             git reset HEAD "$file"
             echo "File removed from commit: $file"
         done
-        read -p "Proceed with your commit for the other files."
+        echo "Commit cancelled | Proceed with your commit for the other files."
         exit 1
     else
         # The user wishes to retain the changes, continue with the commit

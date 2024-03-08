a78a9f809d2d8056a22cd26fb1fb65fbbdddb01b
misantamaria@upm.es <mi.santamaria@upm.es>
2024-03-08 14:28:48 +0000

    added path configurable para probar en get_config

--- clean-jupyter-files.sh
+++ clean-jupyter-files.sh
@@ -7,11 +7,7 @@ for notebook in "$@"; do
     #/usr/local/share/miniconda3/envs/env/bin/nbdev_clean --clear_all --fname "$notebook"
     #--- Do not clean outputs
     echo "--> Clean $notebook "
-<<<<<<< HEAD
-    /usr/local/share/miniconda3/envs/env/bin/nbdev_clean --clear_all --fname "$notebook"
-=======
     /usr/local/share/miniconda3/envs/env/bin/nbdev_clean --fname "$notebook"
->>>>>>> chore-add-pre-commit-hooks-cleaned
     if [ $? -ne 0 ]; then
         echo "Error cleaning $notebook"
     fi

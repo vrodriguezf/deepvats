27085e323d313a9ed85aa5047309e562aaad5f1c
mi.santamaria <mi.santamaria@upm.es>
2024-05-22 09:25:37 +0000

    cambiando a master para arreglar simon issue

--- r_shiny_app/Server-helper.R
+++
@@ -1,9 +0,0 @@
- logMessages <- reactiveVal("")
-    send_log <- function(message) {
-        print("--> Log message")
-        new_log = paste0(logMessages(), Sys.time(), " - ", message, "\n")
-        logMessages(new_log)
-        #print(new_log)
-        invalidateLater(10, session)
-        print(paste0("Log Message |  ", message, "-->"))
-    }

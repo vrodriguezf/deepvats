6715eb5d38ddf398661c0de921019491e8f5f173
mi.santamaria <mi.santamaria@upm.es>
2024-05-23 11:50:53 +0200

    traidas modificaciones de arreglar selección de 2 puntos

--- r_shiny_app/server.R
+++ r_shiny_app/server.R
@@ -1154,17 +1154,19 @@ tcl_1 = Sys.time()
         # Create a reduced window list
         reduced_window_list <-  vector(mode = "list", length = length(idx_window_limits)-1)
         # Populate the first element of the list with the idx of the first window.
-        reduced_window_list[[1]] <- c(unlist_window_indices[idx_window_limits[1]],
-                            unlist_window_indices[idx_window_limits[1+1]])
+        reduced_window_list[[1]] = c(
+            isolate(tsdf())$timeindex[unlist_window_indices[idx_window_limits[1]+1]],
+            isolate(tsdf())$timeindex[unlist_window_indices[idx_window_limits[2]]]
+        ) 
         # Populate the rest of the list
-        for (i in 2:(length(idx_window_limits)-1)){
+        if (length(idx_window_limits) > 2) {
+            for (i in 2:(length(idx_window_limits)-1)){
             reduced_window_list[[i]]<- c(
-                #unlist_window_indices[idx_window_limits[i]+1],
-                #unlist_window_indices[idx_window_limits[i+1]]
-                            as.Date(isolate(tsdf())$timeindex[unlist_window_indices[idx_window_limits[i]+1]]),
-                            as.Date(isolate(tsdf())$timeindex[unlist_window_indices[idx_window_limits[i+1]]])
-                               )
-        }
+                isolate(tsdf())$timeindex[unlist_window_indices[idx_window_limits[i]+1]],
+                isolate(tsdf())$timeindex[unlist_window_indices[idx_window_limits[i+1]]]
+                )
+            }
+            }
         reduced_window_list
     })
     

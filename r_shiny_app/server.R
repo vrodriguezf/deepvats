c795f67e0ce993a037750a8f99fd2800b337ff39
mi.santamaria <mi.santamaria@upm.es>
2024-05-24 15:21:32 +0200

    Guardado para dejar para otro momento

--- r_shiny_app/server.R
+++ r_shiny_app/server.R
@@ -10,6 +10,8 @@
 
 source("./lib/server/logs.R")
 source("./lib/server/plots.R")
+source("./modules/load.R")
+source("./modules/parameters.R")
 
 shinyServer(function(input, output, session) {
     options(shiny.verbose = TRUE)
@@ -70,27 +72,7 @@ shinyServer(function(input, output, session) {
         label = "input_dataset"
     )
     
-    observeEvent(input$dataset, {
-        #req(encs_l)
-        log_print("--> observeEvent input_dataset | update encoder list")
-        log_print(input$dataset)
-        freezeReactiveValue(input, "encoder")
-        log_print(paste0("observeEvent input_dataset | update encoders for dataset ", input$dataset))
-        updateSelectizeInput(
-            session = session,
-            inputId = "encoder",
-            choices = encs_l %>% 
-            keep(~ .$metadata$train_artifact == input$dataset) %>% 
-            #map(~ .$metadata$enc_artifact) %>% 
-            names
-        )
-        ### TODO: Ver cómo poner bien esta ñapa para que no se actualizen los gráficos antes que el stride
-        updateSliderInput(session, "stride", value = 0)
-        ################
-        on.exit(
-            {log_print("observeEvent input_dataset | update encoder list -->"); flush.console()}
-        )
-    }, label = "input_encoder")
+    select_dataset(encs_l, input, output, session)
     
     # observeEvent(input$encoder, {
     #   freezeReactiveValue(input, "embs_ar")

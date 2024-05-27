6934e8ae1cae8b2f6dae6ad1100c7e42f5194e94
mi.santamaria <mi.santamaria@upm.es>
2024-05-27 14:27:51 +0200

    Modularizando embeddings aesthetics para entender bien la estructura de un tab...

--- r_shiny_app/modules/embeddings.R
+++ r_shiny_app/modules/embeddings.R
@@ -1,3 +1,108 @@
+# This module defines the embeddings tab of the App.
+
+###############
+### Helpers ###
+###############
+
+# -- Embeddings aesthetics -- #
+
+path_line_selector <- function(id){
+  sliderInput(
+    "path_line_size", 
+    label = "path_line_size", 
+    value = DEFAULT_VALUES$path_line_size, 
+    min=0, max=5, step = 0.01
+  )
+}
+path_alpha_selector <- function(id){
+  sliderInput(
+    "path_alpha", 
+    label = "path_alpha",
+    value = DEFAULT_VALUES$path_alpha, 
+    min=0, max=1, step = 0.01
+  )
+}
+point_alpha_selector <- function(id){
+  sliderInput(
+    "point_alpha", 
+    label = "point_alpha",
+    value = DEFAULT_VALUES$point_alpha, 
+    min=0, max=1, step = 0.01
+  )
+}
+point_size_selector <- function(id) {
+  sliderInput(
+    "point_size", 
+    label = "point_size",
+    value = DEFAULT_VALUES$point_size, 
+    min=0, max=10, step = 0.5
+  )
+}
+
+embeddings_aesthetics_update <- function(id) {
+  ns <- NS(id)
+  actionBttn(
+    inputId = "update_prj_graph",
+    label = "Update aestethics",
+    style = "simple",
+    color = "primary",
+    icon = icon("bar-chart"),
+    size = "xs", block = TRUE
+  )
+}
+embeddings_aesthetics <- function(id){
+  ns <- NS(id)
+  column(1,
+    dropdownButton(
+      tags$b("Set height of the projections plot (px):"),
+      numericInput("embedding_plot_height", label = "Height",value =400),
+      hr(),
+      tags$b("Configure aestethics"),
+      path_line_selector("path_line_selector"),
+      path_alpha_selector("path_alpha_selector"),
+      point_alpha_selector("point_alpha_selector"),
+      point_size_selector("point_size_selector"),
+      checkboxInput("show_lines", "Show lines", value = TRUE),
+      actionButton('savePlot', 'Save embedding projections plot'),
+      embeddings_aesthetics_update("embeddings_aesthetics_update"),
+      circle = FALSE, status = "primary",
+      icon = icon("gear"), width = "300px",size = "xs",
+      tooltip = tooltipOptions(title = "Configure the embedding appearance"),
+      inputId = "projections_config"
+    )
+  )
+}
+
+# -- Controllers -- #
+embeddings_zoom_button <- function(id){
+  ns <- NS(id)
+  prettyToggle(
+    inputId     = "zoom_btn",
+    label_on    = "Zoom out",
+    label_off   = "Zoom in",
+    shape       = "square",
+    outline     = TRUE,
+    plain       = TRUE,
+    inline      = TRUE,
+    icon_on     = icon("search-minus"), 
+    icon_off    = icon("search-plus"),
+    status_on   = "danger",
+    status_off  = "primary"
+  )
+}
+
+embeddings_plot_windows <- function(id){
+  ns <- NS(id)
+  materialSwitch(
+    inputId  = "plot_windows",
+    label    = "Plot windows",
+    status   = "info",
+    value    = TRUE,
+    inline   = TRUE
+  )
+}
+
+# -- Tab UI function -- #
 embeddings_tabUI <- function(id) {
     ns <- NS(id)
     tabPanel(
@@ -5,52 +110,10 @@ embeddings_tabUI <- function(id) {
           fluidRow(
             h3("Embedding projections"),
             fluidRow(
-              column(1,
-                     dropdownButton(
-                       tags$b("Set height of the projections plot (px):"),
-                       numericInput("embedding_plot_height", label = "Height",value =400),
-                       hr(),
-                       tags$b("Configure aestethics"),
-                       sliderInput("path_line_size", label = "path_line_size", 
-                                   value = DEFAULT_VALUES$path_line_size, min=0, max=5, step = 0.01),
-                       sliderInput("path_alpha", label = "path_alpha",
-                                   value = DEFAULT_VALUES$path_alpha, min=0, max=1, step = 0.01),
-                       sliderInput("point_alpha", label = "point_alpha",
-                                   value = DEFAULT_VALUES$point_alpha, min=0, max=1, step = 0.01),
-                       sliderInput("point_size", label = "point_size",
-                                   value = DEFAULT_VALUES$point_size, min=0, max=10, step = 0.5),
-                       checkboxInput("show_lines", "Show lines", value = TRUE),
-                       actionButton('savePlot', 'Save embedding projections plot'),
-
-                       actionBttn(inputId = "update_prj_graph",label = "Update aestethics",style = "simple",
-                                  color = "primary",icon = icon("bar-chart"),size = "xs", block = TRUE),
-                       circle = FALSE, status = "primary",
-                       icon = icon("gear"), width = "300px",size = "xs",
-                       tooltip = tooltipOptions(title = "Configure the embedding appearance"),
-                       inputId = "projections_config"
-                     )
-              ),
+              embeddings_aesthetics("embeddings_aesthetics"),
               column(8,
-                     prettyToggle(
-                       inputId = "zoom_btn",
-                       label_on = "Zoom out",
-                       label_off = "Zoom in",
-                       shape = "square",
-                       outline = TRUE,
-                       plain = TRUE,
-                       inline = TRUE,
-                       icon_on = icon("search-minus"), 
-                       icon_off = icon("search-plus"),
-                       status_on = "danger",
-                       status_off = "primary"
-                     ),
-                     materialSwitch(
-                       inputId = "plot_windows",
-                       label = "Plot windows",
-                       status = "info",
-                       value = TRUE,
-                       inline = TRUE
-                     )
+                     embeddings_zoom_button("embeddings_zoom_button"),
+                     embeddings_plot_windows("embeddings_plot_windows"),
               ),
               column(3)
             ),

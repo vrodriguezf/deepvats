7b63e38a45a8b3776b7b86065881f9bd0756bfc3
misantamaria@upm.es <mi.santamaria@upm.es>
2024-09-16 15:16:51 +0000

    Ejemplo completo de chronos completado

--- dvats/encoder.py
+++ dvats/encoder.py
@@ -92,6 +92,30 @@ def get_enc_embs_ensure_batch_size_(
             print(f"[ Get Encoder Embeddings Ensure Batch Size ] Batch size proposed. Using {dls.bs}")
 
 # %% ../nbs/encoder.ipynb 15
+from fastai.learner import Learner
+from tsai.data.core import TSDataLoaders
+
+# %% ../nbs/encoder.ipynb 16
+def get_enc_embs_ensure_batch_size_(
+    dls        : TSDataLoaders,
+    batch_size : int = None,
+    verbose    : int = 0
+) -> None:
+    if batch_size is None:
+        if verbose > 1: 
+            print("[ Get Encoder Embeddings Ensure Batch Size ] No batch size proposed")
+        if dls.bs == 0: 
+            if verbose > 1: 
+                print("[ Get Encoder Embeddings Ensure Batch Size ] Using value 64 as 0 is not a valid value.")
+            enc_learn.dls.bs = 64
+        elif verbose > 1: 
+            print(f"[ Get Encoder Embeddings Ensure Batch Size ] Using the original value: {dls.bs}")
+    else:
+        dls.bs = batch_size
+        if verbose > 1: 
+            print(f"[ Get Encoder Embeddings Ensure Batch Size ] Batch size proposed. Using {dls.bs}")
+
+# %% ../nbs/encoder.ipynb 17
 def get_enc_embs(
     X               : List [ List [ List [ float ] ] ], 
     enc_learn       : Learner, 
@@ -176,7 +200,7 @@ def get_enc_embs(
     if to_numpy: embs = embs.numpy() if cpu else embs.cpu().numpy()
     return embs
 
-# %% ../nbs/encoder.ipynb 16
+# %% ../nbs/encoder.ipynb 18
 def get_enc_embs_set_stride_set_batch_size(
     X                  : List [ List [ List [ float ] ] ], 
     enc_learn          : Learner, 
